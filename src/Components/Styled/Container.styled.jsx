302c8f3beefd506ab70e38a07f48d8d504f20738
KmiloLopez <108908318+KmiloLopez@users.noreply.github.com>
2023-04-26 21:13:00 -0500

    first commit

---
+++ src/Components/Styled/Container.styled.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 50px;
+    background-color: lightgrey;
+`
+export default Container
